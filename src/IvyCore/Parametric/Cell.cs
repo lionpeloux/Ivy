e102f07695d6e6d6d3fa022604bac8af4daedb7b
Lionel <lionel.dupeloux@gmail.com>
2016-12-02 19:41:21 -0500

    Detailed help comment on multi linear interpolation

--- src/IvyCore/Parametric/Cell.cs
+++ src/IvyCore/Parametric/Cell.cs
@@ -66,29 +66,53 @@ namespace IvyCore.Parametric
             ///     D=1             D=2             D=3
             /// 
             ///     base:  (i)      base:  (i,j)    base:  (i,j,k)
-            ///     i=0 : +(0)      i=0 : +(0,0)    i=0 : +(0,0,0)
-            ///     i=1 : +(1)      i=1 : +(1,0)    i=1 : +(1,0,0)
-            ///                     i=2 : +(0,1)    i=2 : +(0,1,0)
-            ///                     i=3 : +(1,1)    i=3 : +(1,1,0)
-            ///                                     i=4 : +(0,0,1)
-            ///                                     i=5 : +(1,0,1)
-            ///                                     i=6 : +(0,1,1)
-            ///                                     i=7 : +(1,1,1)
+            ///     p=0 : +(0)      p=0 : +(0,0)    p=0 : +(0,0,0)
+            ///     p=1 : +(1)      p=1 : +(1,0)    p=1 : +(1,0,0)
+            ///                     p=2 : +(0,1)    p=2 : +(0,1,0)
+            ///                     p=3 : +(1,1)    p=3 : +(1,1,0)
+            ///                                     p=4 : +(0,0,1)
+            ///                                     p=5 : +(1,0,1)
+            ///                                     p=6 : +(0,1,1)
+            ///                                     p=7 : +(1,1,1)
             /// 
             /// On note :
             ///     
-            ///     i   :   l'indice de la permutation i ∈ [0,2^D - 1]
+            ///     i       l'indice de la permutation i ∈ [0,2^D - 1]
             ///             peut également être reprenté par un tuple de dimension D 
-            ///             avec des 0 ou 1 (cf example)
+            ///             
+            ///     tup     la représentation de la permutation sous forme de tuple
+            ///             tup = (σ{0}, σ{1}, ..., σ{D-1}) avec σ = [0,1]
             /// 
-            ///     X{i}:   le noeud i de coordoonées [x{i}_1, x{i}_2, ..., x{i}_{D-1}] ∈ R^D
+            ///     X{i}    le noeud i de coordoonées [x0{i}, x1{i}, ..., x{D-1}{i}] ∈ R^D
+            ///             en un noeud chaque coordonnée ne prend que l'une des 2 valeurs
+            ///             x{k}{i} in [x{k}_0, x{k}_1] k ∈ [0,D-1]
+            ///             
+            ///     X       le point à interpoler dans l'espace du polytope et de coordonnées
+            ///             [x0, x1, ..., x{D-1}] ∈ R^D
             ///     
-            ///     Y{i}:   la valeur du champs ∈ R^n (n > 0) à interpoler au noeud X{i}
+            ///     Y{i}    la valeur du champs à interpoler (Y ∈ R^n) au noeud X{i}
+            ///             [y0{i}, y1{i}, ..., y{n-1}{i}] ∈ R^n
             ///             Y{i} = f(X{i}) avec f : R^D -> R^n 
-        
-            ///     V   :   le volume du polytope = 
-
-
+            ///     
+            ///     Y       le champ interpolé au point X
+            ///             [y0, y1, ..., y{n-1}] ∈ R^n
+            ///             
+            ///     V       le volume du polytope
+            ///             V = (x0_1 - x0_0) * (x1_1 - x1_0) * ... * (x{D-1}_1 - x{D-1}_0)
+            ///             
+            ///     V{i}    le volume de la partition associée à la permutation i
+            ///             V{i} = PROD[ 
+            ///                          x{k}_1 - x{k}     | si σ{k} = 0
+            ///                          x{k}   - x{k}_0)  | si σ{k} = 1
+            ///                         ] , k ∈ [0, D-1]
+            ///                         
+            ///     N{i}    le volume normalisé N{i} = V{i}/V associé à la permutation i
+            ///     
+            /// On calcul :
+            /// 
+            ///     Y = Y{0} * N{0} +  Y{1} * N{1} + ... + Y{2^D -1} * N{2^D -1}
+            ///
+            
             for (int i = 0; i < nperm; i++)
             {
                 var perm = Grid.Permutations[i];
